868bb9b247fa0f35c2e7b5b65efec359935a7b97
leora-Nt <103191007+leora-Nt@users.noreply.github.com>
2022-05-01 06:44:57 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -32,13 +32,13 @@ async def start_(client: Client, message: Message):
     )
 
 
-
 @Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
-async def ping_pong(c: Client, message: Message):
+async def ping_pong(client: Client, message: Message):
     start = time()
     m_reply = await message.reply_text("pinging...")
     delta_ping = time() - start
-    await m_reply.edit_text("**× I am Alive**")
+    await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
+
 
 
 @Client.on_message(command(["repo"]) & ~filters.edited)
